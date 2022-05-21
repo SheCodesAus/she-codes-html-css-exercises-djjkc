c0d7b3406f0f3e443eb2f5d94455e7a2ff07a9db
djjkc <102584654+djjkc@users.noreply.github.com>
2022-05-21 12:36:19 +1000

    JS Start

--- intro-to-js/script.js
+++ intro-to-js/script.js
@@ -1,6 +1,17 @@
 window.onload = function() {
     // start
+    let starterDiv = document.getElementById('starter-div')
+    let firstHeading = document.createElement('h2')
+    firstHeading.innerText = 'This is a H2 created with Javascript'
+    firstHeading.classList.add('js-header')
 
+    starterDiv.appendChild(firstHeading)
+
+    let secondHeading = document.createElement('h2')
+    secondHeading.innerText = "This is another H2 created with JS!"
+    secondHeading.classList.add('js-header')
+
+    starterDiv.appendChild(secondHeading)
     // end
 }
 
@@ -8,7 +19,7 @@ window.onload = function() {
 function showName(){
     console.log('showName');
     // start
-
+    let name = document.getElementById('name').value;document.getElementById('greeting-text').innerText = 'Hi ' + name;
     // end
 }
 
